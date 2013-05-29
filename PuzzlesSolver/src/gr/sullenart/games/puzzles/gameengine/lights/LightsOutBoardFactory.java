ad472c31c020938725b81e79cec6dbdd766f93a6
Konstantinos Giannakakis <kgiannak1974@gmail.com>
2013-05-29 09:00:54 +0300

    Start only solvable boards

--- PuzzlesSolver/src/gr/sullenart/games/puzzles/gameengine/lights/LightsOutBoardFactory.java
+++ PuzzlesSolver/src/gr/sullenart/games/puzzles/gameengine/lights/LightsOutBoardFactory.java
@@ -11,9 +11,21 @@ public class LightsOutBoardFactory {
     
     public int [] getBoard(int sizeX, int sizeY) {
         int [] board = new int[sizeX * sizeY];
+        boolean isSolved = false;
         
-        for (int i=0;i<board.length;i++) {
-            board[i] = rnd.nextInt(2);
+        while(!isSolved) {
+        	int lightsCount = 0;
+	        for (int i=0;i<board.length;i++) {
+	            board[i] = rnd.nextInt(2);
+	            lightsCount++;
+	        }
+	        if (lightsCount == 0) {
+	        	continue;
+	        }
+	        LightsOutBoard lightsOutBoard = 
+	        	new LightsOutBoard(sizeX, sizeY, board);
+	        LightsOutSolver solver = new LightsOutSolver(lightsOutBoard, 2, 0);
+	        isSolved = solver.solve();
         }
         
         return board;
